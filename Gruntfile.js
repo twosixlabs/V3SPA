8ecc517e4dce7f073619a1bc4cd34b24167cd67f
Chris Wacek <chris.wacek@invincea.com>
2014-01-13 14:10:57 -0500

    Added spinner to indicate loading.

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,6 +46,7 @@ module.exports = function(grunt) {
         files: [
           {'static/js/lobster-json.js': 'external/node-json-lobster/dist/lobster-json.js'},
           {'static/js/sockjs.0.3.min.js': 'external/sockjs.0.3.min.js'},
+          {'static/js/spin.min.js': 'external/spin.min.js'},
           { 
             expand: true, cwd: 'external/angular', 
             dest: 'static/js/', src: 'angular*.js'
