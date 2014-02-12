c0668b2f48bde5c9bf2b5de10504ec85933eeb26
Chris Wacek <chris.wacek@invincea.com>
2014-02-12 08:01:59 -0500

    Use less to import Avispa styles

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,19 @@ var include = require('gulp-include')
 var rename = require('gulp-rename')
 var uglify = require('gulp-uglify')
 var debug = require('gulp-debug')
+var less = require("gulp-less")
 
 var paths = {
-  html: ['src/index.html', 'src/partials/*.html']
+  html: ['src/index.html', 'src/partials/*.html'],
+  less: ['src/less/*.less'],
 }
 
-gulp.task('scripts', function(){
+
+
+/* Build the application files by including
+ * all literate coffeescript files, compiling
+ * them and then including all javascript */
+gulp.task('application', function(){
   gulp.src('src/js/app.litcoffee')
   .pipe(include({
       extensions:  "litcoffee"
@@ -25,6 +32,8 @@ gulp.task('scripts', function(){
   .pipe(gulp.dest('static/js'))
 })
 
+/* Build all assets using include, output
+ * them both minified and not minified */
 gulp.task("script_assets", function() {
   gulp.src('external/assets.js')
   .pipe(include({extensions: 'js'}))
@@ -38,15 +47,23 @@ gulp.task("script_assets", function() {
   .pipe(gulp.dest('static/js'))
 })
 
+/* Copy HTML files to the static directory */
 gulp.task('html', function(){
   gulp.src(paths.html)
   .pipe(gulp.dest('static'))
 })
 
+/* Run the LESS preprocessor */
+gulp.task('less', function() {
+  gulp.src('src/less/vespa.less')
+  .pipe(less())
+  .pipe(gulp.dest('static/less'))
+})
+
 gulp.task('reloader', function() {
   gulp.watch(paths.html, [ 'html' ])
   gulp.watch('external/assets.js', [ 'script_assets' ])
-  gulp.watch(['src/**/*coffee'], [ 'scripts' ])
+  gulp.watch(['src/**/*coffee'], [ 'application' ])
 })
 
 gulp.task('default', ['scripts', 'script_assets', 'html'])
