2383da48cd357e3b4790d6175e532eabc5429b1b
Robert Gove <robert.gove@invincea.com>
2015-10-23 14:34:38 -0400

    Returning rules for all modules

--- api/handlers/ws_domains/raw.py
+++ api/handlers/ws_domains/raw.py
@@ -33,7 +33,7 @@ class RawDomain(object):
 
         # If the raw is identical, and the parameters are identical, just return the one we already
         # translated.
-        if (refpol.parsed
+        if (False and refpol.parsed
                 and refpol.documents['raw']['digest'] == raw_hash
                 and refpol.parsed['params'] == msg['payload']['params']):
 
@@ -46,21 +46,40 @@ class RawDomain(object):
 
         else:
 
+            table = []
+
+            # Need to get the parsed data for all the modules
             for modname in refpol['modules']:
                 # Use ws_domains.call() to invoke raw.py and get the raw policy
                 module = refpol['modules'][modname]
+                with open(module['te_file'], 'r') as f:
+                    for line in f:
+                        if line.lstrip(' \t\n\r').startswith("allow"):
+                            rule = line.strip().split(" ")
+                            #TODO:Need to handle lists of perms, right now this assumes only one perm
+                            if "{" in rule[3]:
+                                rule[3] = line.split("{")[1].rstrip(' \t\n\r').rstrip(' };')
+                            obj_t = rule[2].split(":")[0]
+                            if obj_t.lower() == "self":
+                                obj_t = rule[1]
+                            obj_c = rule[2].split(":")[1]
+                            row = {"subject":rule[1],"object":obj_t,"class":obj_c,"perms":rule[3].strip(),"module":modname}
+                            # Also save the entire rule as text
+                            row["rule"] = line.lstrip().rstrip('\n')
 
-            exec_str = "./ide/tools/te2json.py"
-            exec_str += " -j -t -i"
-            #filename = "~/Documents/V3SPA/ide/tools/" + (params['filename'] if params['filename'] else "apache.te")
-            filename = module['te_file']
-            exec_str += " " + filename
-            output = Popen([exec_str], stdout=PIPE, shell=True).communicate()[0]
+                            table.append(row)
+
+            # exec_str = "./ide/tools/te2json.py"
+            # exec_str += " -j -t -i"
+            # #filename = "~/Documents/V3SPA/ide/tools/" + (params['filename'] if params['filename'] else "apache.te")
+            # filename = module['te_file']
+            # exec_str += " " + filename
+            # output = Popen([exec_str], stdout=PIPE, shell=True).communicate()[0]
 
             refpol['parsed'] = {
                 'version': '1.0',
                 'errors': [],
-                'parameterized': api.db.json.loads(output),
+                'parameterized': {"rules": table},
                 'params': msg['payload']['params']
             }
 
