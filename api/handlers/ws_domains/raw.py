dfc515ff71db7b63d027410f1dd865bef854df57
Robert Gove <robert.gove@invincea.com>
2016-04-26 10:39:17 -0400

    Parsing raw policy text from the database

--- api/handlers/ws_domains/raw.py
+++ api/handlers/ws_domains/raw.py
@@ -51,72 +51,73 @@ class RawDomain(object):
             table = []
 
             # Need to get the parsed data for all the modules
-            for modname in refpol['modules']:
-                # Use ws_domains.call() to invoke raw.py and get the raw policy
-                module = refpol['modules'][modname]
-                with open(module['te_file'], 'r') as f:
-                    line_num = 0
-                    for line in f:
-                        line_num += 1
-                        if line.lstrip(' \t\n\r').startswith("allow"):
-                            # Skip allow rules that do not have colons
-                            if line.find(":") == -1:
-                                continue
+            # Use ws_domains.call() to invoke raw.py and get the raw policy
+            print(raw)
+            module = "refpol['modules'][modname]"
+            line_num = 0
+            for line in raw.splitlines():
+                line_num += 1
+                if line.lstrip(' \t\n\r').startswith("allow"):
+                    # Skip allow rules that do not have colons
+                    if line.find(":") == -1:
+                        continue
 
-                            # Skip allow rules that do not have semicolons (e.g. multiline rules)
-                            if line.find(";") == -1:
-                                continue
+                    # Skip allow rules that do not have semicolons (e.g. multiline rules)
+                    if line.find(";") == -1:
+                        continue
 
-                            # Replace sequences of spaces with a single space, trim, and split on ":"
-                            rule = re.sub(" +", " ", line.strip()).split(":")
+                    # Replace sequences of spaces with a single space, trim, and split on ":"
+                    rule = re.sub(" +", " ", line.strip()).split(":")
 
-                            lside = rule[0].lstrip("allow").strip()
-                            rside = rule[1].strip()
+                    lside = rule[0].lstrip("allow").strip()
+                    rside = rule[1].strip()
 
-                            if lside.find("{") == -1:
-                                # Should be "subj_t obj_t;"
-                                subj_t = lside.split(" ")[0]
-                                obj_t = lside.split(" ")[1]
-                            elif lside.find("{") < lside.find(" "):
-                                # We have a list of subjects
-                                subj_t = lside[lside.find("{")+1:lside.find("}")].strip()
-                                if lside.find("}") == lside.rfind("}"):
-                                    # {subj_t1 subj_t2} obj_t;
-                                    obj_t = lside.split("}")[1].rstrip(";").strip()
-                                else:
-                                    # {subj_t1 subj_t2} {obj_t1 obj_t2};
-                                    obj_t = lside[lside.rfind("{")+1:lside.rfind("}")].strip()
-                            else:
-                                # subj_t {obj_t1 obj_t2};
-                                subj_t = lside.split("{")[0].strip()
-                                obj_t = lside[lside.find("{")+1:lside.find("}")].strip()
+                    if lside.find("{") == -1:
+                        # Should be "subj_t obj_t;"
+                        subj_t = lside.split(" ")[0]
+                        obj_t = lside.split(" ")[1]
+                    elif lside.find("{") < lside.find(" "):
+                        # We have a list of subjects
+                        subj_t = lside[lside.find("{")+1:lside.find("}")].strip()
+                        if lside.find("}") == lside.rfind("}"):
+                            # {subj_t1 subj_t2} obj_t;
+                            obj_t = lside.split("}")[1].rstrip(";").strip()
+                        else:
+                            # {subj_t1 subj_t2} {obj_t1 obj_t2};
+                            obj_t = lside[lside.rfind("{")+1:lside.rfind("}")].strip()
+                    else:
+                        # subj_t {obj_t1 obj_t2};
+                        subj_t = lside.split("{")[0].strip()
+                        obj_t = lside[lside.find("{")+1:lside.find("}")].strip()
 
-                            if rside.find("{") == -1:
-                                # Should be "obj_c perm;"
-                                obj_c = rside.split(" ")[0]
-                                perms = rside.split(" ")[1].rstrip(";")
-                            elif rside.find("{") < rside.find(" "):
-                                # We have a list of classes
-                                obj_c = rside[rside.find("{")+1:rside.find("}")].strip()
-                                if rside.find("}") == rside.rfind("}"):
-                                    # {obj_c1 obj_c2} permission;
-                                    perms = rside.split("}")[1].rstrip(";").strip()
-                                else:
-                                    # {obj_c1 obj_c2} {perm1 perm2};
-                                    perms = rside[rside.rfind("{")+1:rside.rfind("}")].strip()
-                            else:
-                                # obj_c {perm1 perm2};
-                                obj_c = rside.split("{")[0].strip()
-                                perms = rside[rside.find("{")+1:rside.find("}")].strip()
+                    if rside.find("{") == -1:
+                        # Should be "obj_c perm;"
+                        obj_c = rside.split(" ")[0]
+                        perms = rside.split(" ")[1].rstrip(";")
+                    elif rside.find("{") < rside.find(" "):
+                        # We have a list of classes
+                        obj_c = rside[rside.find("{")+1:rside.find("}")].strip()
+                        if rside.find("}") == rside.rfind("}"):
+                            # {obj_c1 obj_c2} permission;
+                            perms = rside.split("}")[1].rstrip(";").strip()
+                        else:
+                            # {obj_c1 obj_c2} {perm1 perm2};
+                            perms = rside[rside.rfind("{")+1:rside.rfind("}")].strip()
+                    else:
+                        # obj_c {perm1 perm2};
+                        obj_c = rside.split("{")[0].strip()
+                        perms = rside[rside.find("{")+1:rside.find("}")].strip()
 
-                            for s in subj_t.split(" "):
-                                for ot in obj_t.split(" "):
-                                    for oc in obj_c.split(" "):
-                                        for p in perms.split(" "):
-                                            row = {"subject":s, "object":ot, "class":oc, "perm":p, "module":modname, "line": line_num}
-                                            row["directory"] = os.path.basename(os.path.dirname(f.name))
-                                            row["rule"] = line.lstrip().rstrip('\n')
-                                            table.append(row)
+                    for s in subj_t.split(" "):
+                        for ot in obj_t.split(" "):
+                            for oc in obj_c.split(" "):
+                                for p in perms.split(" "):
+                                    row = {"subject":s, "object":ot, "class":oc, "perm":p, "module":modname, "line": line_num}
+                                    row["directory"] = os.path.basename(os.path.dirname(f.name))
+                                    row["rule"] = line.lstrip().rstrip('\n')
+                                    table.append(row)
+                else:
+                    raise Exception("Could not parse line {0}: {1}".format(line_num, line))
 
             # exec_str = "./ide/tools/te2json.py"
             # exec_str += " -j -t -i"
